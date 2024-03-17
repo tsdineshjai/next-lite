8b2bb5d85141da2436ab5bd87f32dd5c16bc587f
tanishka <tsdineshjai@gmail.com>
2024-03-17 17:56:05 +0530

    next initiated

---
+++ src/app/footer/page.js
@@ -0,0 +1,6 @@
+function page() {
+	const timeStamp = new Date().toLocaleDateString();
+	return <footer>page</footer>;
+}
+
+export default page;
---
+++ src/app/welcome/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Welcome() {
+	return <div>Welcome to Next Js Jungle</div>;
+}
+
+export default Welcome;
